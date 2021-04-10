3ce974b76cb07b893318943b39452eea0f55ab36
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-04-10 17:09:46 +0600

    + url

--- video-stream-viewer/src/app/video-player/video-player.component.ts
+++ video-stream-viewer/src/app/video-player/video-player.component.ts
@@ -14,6 +14,6 @@ export class VideoPlayerComponent implements OnInit {
   url: string | null = null;
 
   ngOnInit(): void {
-    this.url = "https://cloud2.macroscop.com:18080/video?channelid=3329c69b-17e4-4d3b-9fcc-542bdfaefbe1&login=user3141400&password=78302615C8B79CAC8DF6D2607F8A83EE";
+    this.url = "https://cloud2.macroscop.com:18080/mobile?mode=archive&startTime=31.03.2021%2B02%3A10%3A12&speed=1&channelid=3329c69b-17e4-4d3b-9fcc-542bdfaefbe1&login=user3141400&password=78302615C8B79CAC8DF6D2607F8A83EE";
   }
 }
