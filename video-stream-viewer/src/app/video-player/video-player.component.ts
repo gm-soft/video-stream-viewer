e5fdecd98b7f2b8d860f077f64c416031a620666
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-04-13 19:50:44 +0600

    Have added websockets

--- video-stream-viewer/src/app/video-player/video-player.component.ts
+++ video-stream-viewer/src/app/video-player/video-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { webSocket } from "rxjs/webSocket";
 
 @Component({
   selector: 'app-video-player',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-player.component.scss']
 })
 export class VideoPlayerComponent implements OnInit {
+  constructor() {
+    this.initWebSocketSubject();
+  }
+
+  private uuidv4(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
   get show(): boolean {
     return this.url != null;
@@ -14,6 +25,64 @@ export class VideoPlayerComponent implements OnInit {
   url: string | null = null;
 
   ngOnInit(): void {
-    this.url = "https://cloud2.macroscop.com:18080/mobile?mode=archive&startTime=31.03.2021%2B02%3A10%3A12&speed=1&channelid=3329c69b-17e4-4d3b-9fcc-542bdfaefbe1&login=user3141400&password=78302615C8B79CAC8DF6D2607F8A83EE";
+    this.url = '';
+  }
+
+  // ---------------
+  private initWebSocketSubject() {
+    const subject = webSocket({
+      url: 'wss://cloud2.macroscop.com:18080/webapi/ws',
+      deserializer: (e) => e,
+      serializer: (value) => JSON.stringify(value),
+    });
+
+    subject.subscribe(
+      msg =>{
+        // Called whenever there is a message from the server.
+        console.log(msg); 
+      },
+      err => {
+        // Called if at any point WebSocket API signals some kind of error.
+        console.log(err);
+      }, 
+      () => console.log('complete') // Called when connection is closed (for whatever reason).
+    );
+
+    subject.next({
+      commandType: 0,
+      parameters: {
+        password: "78302615C8B79CAC8DF6D2607F8A83EE",
+        userName: "user3141400",
+        userType: 0
+      },
+      requestId: this.uuidv4()
+    });
+
+    subject.next({
+      parameters: {
+        archiveParameters: null,
+        cameraId: "3329c69b-17e4-4d3b-9fcc-542bdfaefbe1",
+        displayParameters: {
+          fps: 15,
+          width: 447,
+          height: 648,
+          isFullScreen: false
+        },
+        soundStreamParameters: {
+          isSoundOn: false,
+          soundFormat: 0
+        },
+        isSoundOn: false,
+        soundFormat: 0,
+        videoStreamParameters: {
+          streamType: "main",
+          streamFormat: "mjpeg"
+        },
+        streamFormat: "mjpeg",
+        streamType: "main",
+      },
+      requestId: this.uuidv4(),
+      commandType: 1,
+    });
   }
 }
